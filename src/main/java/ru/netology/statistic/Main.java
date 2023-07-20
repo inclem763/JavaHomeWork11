0967a39823ec1319c8364529303fe810a9d57d7f
Dmitriy Kozak <dmitrij.kozak.1997@bk.ru>
2023-07-20 18:12:21 +0400

    Main, PosterManager and PosterManagerTest added

---
+++ src/main/java/ru/netology/statistic/Main.java
@@ -0,0 +1,15 @@
+package ru.netology.statistic;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] names = {
+                "Бладшот",
+                "Вперед",
+                "Отель <Белград>",
+                "Джентельмены",
+                "Человек-невидимка",
+                "Тролли.Мировой тур",
+                "Номер один"
+        };
+    }
+}
---
+++ src/main/java/ru/netology/statistic/PosterManager.java
@@ -0,0 +1,41 @@
+package ru.netology.statistic;
+
+public class PosterManager {
+    private String[] movies = new String[0];
+    private int defaultCountMovie;
+
+    public PosterManager(int defaultCountMovie) {
+        this.defaultCountMovie = defaultCountMovie;
+    }
+
+    public PosterManager() {
+        this.defaultCountMovie = 10;
+    }
+
+    public void add(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
+    }
+
+    public String[] findAll() {
+        return movies;
+    }
+
+    public String[] findLast() {
+        int resultLength;
+        if (movies.length < defaultCountMovie) {
+            resultLength = movies.length;
+        } else {
+            resultLength = defaultCountMovie;
+        }
+        String[] tmp = new String[resultLength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
+        }
+        return tmp;
+    }
+}
